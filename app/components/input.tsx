8ad906926b11471a467e3d793a95f684f3d6d460
Mehmet <onuur.s@gmail.com>
2025-01-31 18:13:27 +0100

    feat: implemented styling

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,5 +1,11 @@
 interface Props extends React.ButtonHTMLAttributes<HTMLInputElement> {}
 
 export const Input: React.FC<Props> = (props) => {
-  return <input {...props} />;
+  return (
+    <input
+      placeholder="Search by destination"
+      className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-schiphol-blue-300 focus:border-schiphol-blue-300 transition"
+      {...props}
+    />
+  );
 };
