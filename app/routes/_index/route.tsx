dfe5aa84b89bd25a300c2b11ae52fb2503586980
Mehmet <onuur.s@gmail.com>
2025-01-31 18:15:07 +0100

    feat: added container and set html head tags

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,23 +1,25 @@
-import type { MetaFunction } from "@remix-run/node";
-import { Button } from "~/components/button";
-import { Input } from "~/components/input";
+import { type MetaFunction } from "@remix-run/node";
+import { Container } from "~/components/container";
+import { Typography } from "~/components/typography";
+import { isFetcherValidResponse } from "~/helpers/is-fetcher-valid-response";
 import { FlightCard } from "./flight-card";
+import { SearchForm } from "./search-form";
 import { useSearchFlightsFetcher } from "./use-search-flights-fetcher";
 import { Spinner } from "~/components/spinner";
+import { PageMessage } from "~/components/page-message";
+import { IconMoodEmpty } from "@tabler/icons-react";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "Schiphol - Departures" }];
 };
 
 export default function Index() {
-  const { fetcher, isLoading } = useSearchFlightsFetcher();
+  const { fetcher, isLoading, isEmpty } = useSearchFlightsFetcher();
 
   return (
-    <main>
-      <div>
+    <Container>
+      <main className="py-6">
+        <div>
           <Typography variant="h1" className="mb-6">
             Departures
           </Typography>
@@ -28,11 +30,11 @@ export default function Index() {
             {!isLoading &&
               isFetcherValidResponse(fetcher) &&
               fetcher.data?.flights.map((flight) => {
-              return (
+                return (
                   <FlightCard key={flight.flightIdentifier} flight={flight} />
-              );
-            })}
-      </div>
+                );
+              })}
+          </div>
           {isEmpty && (
             <PageMessage
               icon={IconMoodEmpty}
@@ -41,6 +43,7 @@ export default function Index() {
             />
           )}
         </div>
-    </main>
+      </main>
+    </Container>
   );
 }
