46e49a22eb1184db300db4db9967bee5f52744c0
Mehmet <onuur.s@gmail.com>
2025-01-31 18:08:49 +0100

    feat: added isFetcherValidResponse util function

---
+++ app/helpers/is-fetcher-valid-response.ts
@@ -0,0 +1,9 @@
+import { FetcherWithComponents } from "@remix-run/react";
+import { isFetcherErrorResponse } from "./is-fetcher-error-response";
+import { ErrorResponse } from "./makeEnhancedLoader";
+
+export function isFetcherValidResponse<T>(
+  fetcher: FetcherWithComponents<T>
+): fetcher is FetcherWithComponents<Exclude<T, ErrorResponse>> {
+  return !isFetcherErrorResponse(fetcher);
+}
