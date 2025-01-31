a5502fc9f7937c9498fc0d9a98f4c869f7246e5a
Mehmet <onuur.s@gmail.com>
2025-01-31 18:13:38 +0100

    feat: implemented styling

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,5 +1,10 @@
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const Button: React.FC<Props> = (props) => {
-  return <button {...props} />;
+  return (
+    <button
+      className="px-6 py-3 text-white bg-schiphol-blue-500 rounded-lg shadow-md hover:bg-schiphol-blue-400 transition"
+      {...props}
+    />
+  );
 };
