5b5c3573950eac9104da10c0c2e3559a07d7d3ef
Mehmet <onuur.s@gmail.com>
2025-01-31 18:11:03 +0100

    refactor: updated ui for displaying flights

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,7 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Button } from "~/components/button";
 import { Input } from "~/components/input";
+import { FlightCard } from "./flight-card";
 import { useSearchFlightsFetcher } from "./use-search-flights-fetcher";
+import { Spinner } from "~/components/spinner";
 
 export const meta: MetaFunction = () => {
   return [
@@ -20,37 +22,15 @@ export default function Index() {
           <Input name="q" />
           <Button type="submit">search</Button>
         </fetcher.Form>
-        {isLoading && <h1>Loading...</h1>}
-        <table>
-          <thead>
-            <tr>
-              <th>flightIdentifier</th>
-              <th>flightNumber</th>
-              <th>airport</th>
-              <th>date</th>
-              <th>expectedTime</th>
-              <th>originalTime</th>
-              <th>url</th>
-              <th>score</th>
-            </tr>
-          </thead>
-          <tbody>
-            {fetcher.data?.map((flight) => {
+          <div className="flex flex-col gap-y-5 mt-4">
+            {isLoading && <Spinner />}
+            {!isLoading &&
+              isFetcherValidResponse(fetcher) &&
+              fetcher.data?.flights.map((flight) => {
               return (
-                <tr key={flight.flightNumber}>
-                  <td>{flight.flightIdentifier}</td>
-                  <td>{flight.flightNumber}</td>
-                  <td>{flight.airport}</td>
-                  <td>{flight.date}</td>
-                  <td>{flight.expectedTime}</td>
-                  <td>{flight.originalTime}</td>
-                  <td>{flight.url}</td>
-                  <td>{flight.score}</td>
-                </tr>
+                  <FlightCard key={flight.flightIdentifier} flight={flight} />
               );
             })}
-          </tbody>
-        </table>
       </div>
     </main>
   );
