7e19edebc392b0312735dc7804a002cd6349d1b4
Mehmet <onuur.s@gmail.com>
2025-02-02 17:15:58 +0100

    feat: allow for div props to be used

--- app/components/page-message.tsx
+++ app/components/page-message.tsx
@@ -1,7 +1,7 @@
 import { TablerIcon } from "@tabler/icons-react";
 import { Typography } from "./typography";
 
-interface Props {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
   icon: TablerIcon;
   header: string;
   content: string;
@@ -11,9 +11,13 @@ export const PageMessage: React.FC<Props> = ({
   icon: Icon,
   header,
   content,
+  ...componentProps
 }) => {
   return (
-    <div className="aspect-video bg-gray-200 rounded-lg flex-col flex items-center justify-center text-gray-300">
+    <div
+      className="aspect-video bg-gray-200 rounded-lg flex-col flex items-center justify-center text-gray-300"
+      {...componentProps}
+    >
       <Icon size={120} />
       <Typography variant="h2" className="mt-2">
         {header}
